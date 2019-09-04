71574470f8da092fbe1ca3ea660b9a4ee8fd58b1
supecan <andreivn@yandex.ru>
2019-09-04 13:16:58 -0500

    add new template

--- data_types.py
+++ data_types.py
@@ -6,4 +6,10 @@ access_template = ['switchport mode access',
                    'spanning-tree portfast',
                    'spanning-tree bpduguard enable'
                     'spanning-tree bpduguard enable']
+access_template2 = ['switchport mode access',
+                   'switchport access vlan {}',
+                   'switchport nonegotiate',
+                   'spanning-tree portfast',
+                   'spanning-tree bpduguard enable'
+                    'spanning-tree bpduguard enable']
 print('\n'.join(access_template).format(5))
