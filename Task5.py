bad52635126903cc1ce54c606fbee34984e7cef2
supecan <andreivn@yandex.ru>
2019-09-04 14:18:47 -0500

    Task 5.1

---
+++ Task5.py
@@ -0,0 +1,28 @@
+'''from sys import argv
+name_r = input("Entername Router: ")
+london_co = {
+    'r1': {
+        'location': '21 New Globe Walk',
+        'vendor': 'Cisco',
+        'model': '4451',
+        'ios': '15.4',
+        'ip': '10.255.0.1'
+    },
+    'r2': {
+        'location': '21 New Globe Walk',
+        'vendor': 'Cisco',
+        'model': '4451',
+        'ios': '15.4',
+        'ip': '10.255.0.2'
+    },
+    'sw1': {
+        'location': '21 New Globe Walk',
+        'vendor': 'Cisco',
+        'model': '3850',
+        'ios': '3.6.XE',
+        'ip': '10.255.0.101',
+        'vlans': '10,20,30',
+        'routing': True
+    }
+}
+print(london_co[name_r])'''
