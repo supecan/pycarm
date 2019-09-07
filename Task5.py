f33fb4b5450d4b0686e30b8230a3b9dec20c4b72
supecan <andreivn@yandex.ru>
2019-09-07 16:32:03 -0500

    Task 5.2

--- Task5.py
+++ Task5.py
@@ -1,5 +1,5 @@
 from sys import argv
-name_r = input("Enter name Router: ")
+'''name_r = input("Enter name Router: ")
 name_param = input("Inter name parametr(ios, model, vendor, location, ip)".lower())
 london_co = {
     'r1': {
@@ -26,5 +26,32 @@ london_co = {
         'routing': True
     }
 }
-'''print(london_co[name_r])'''
-print(london_co[name_r][name_param])
+print(london_co[name_r])
+print(london_co[name_r][name_param])'''
+
+
+IP_adress= input("Enter IP adres").replace('/','.')
+IP_adress = IP_adress.split(".")
+int(IP_adress[0])
+int(IP_adress[1])
+int(IP_adress[2])
+int(IP_adress[3])
+mask = IP_adress.pop(-1)
+mask = int(mask)
+msk = (mask * '1') + ((32 - mask) * "0")
+print(msk)
+
+
+ip_template = '''
+    ...: IP address:
+    ...: {:<08b} {:<08b} {:<08b} {:<08b}
+    ...: '''
+
+mask_template = '''
+    ...: Mask:
+    ...: {} {} {} {}
+    ...: '''
+
+
+print(ip_template.format(int(IP_adress[0]), int(IP_adress[1]),int(IP_adress[2]), int(IP_adress[3])))
+print(mask_template.format(msk[0:8], msk[8:16], msk[16:24], msk[24:32]))
