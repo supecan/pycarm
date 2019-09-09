b0611468732a6545bb276f44c3befd5170c8c8b4
supecan <andreivn@yandex.ru>
2019-09-09 12:42:39 -0500

    Task 5.2b

--- Task5.py
+++ Task5.py
@@ -1,36 +1,7 @@
 from sys import argv
-'''name_r = input("Enter name Router: ")
-name_param = input("Inter name parametr(ios, model, vendor, location, ip)".lower())
-london_co = {
-    'r1': {
-        'location': '21 New Globe Walk',
-        'vendor': 'Cisco',
-        'model': '4451',
-        'ios': '15.4',
-        'ip': '10.255.0.1'
-    },
-    'r2': {
-        'location': '21 New Globe Walk',
-        'vendor': 'Cisco',
-        'model': '4451',
-        'ios': '15.4',
-        'ip': '10.255.0.2'
-    },
-    'sw1': {
-        'location': '21 New Globe Walk',
-        'vendor': 'Cisco',
-        'model': '3850',
-        'ios': '3.6.XE',
-        'ip': '10.255.0.101',
-        'vlans': '10,20,30',
-        'routing': True
-    }
-}
-print(london_co[name_r])
-print(london_co[name_r][name_param])'''
 
-
-IP_adress= input("Enter IP adres").replace('/','.')
+Ip_adress = argv[1]
+IP_adress= Ip_adress.replace('/','.')
 IP_adress = IP_adress.split(".")
 int(IP_adress[0])
 int(IP_adress[1])
